fba81affb2ae77a5bd141673d83a9f3e4786ee3c
Param Siddharth <contact@paramsid.com>
2022-06-05 23:35:46 +0530

    Create API to add campaigns
    Signed-off-by: Param Siddharth <contact@paramsid.com>

--- server/campaigns.js
+++ server/campaigns.js
@@ -5,30 +5,47 @@ import * as auth from './auth';
 
 const router = express.Router();
 
-router.get('/', auth.middleware, async (req, res) => {
-  const userID = req.auth.id;
-  const data = await db('campaigns').select('*');
-  for (const item of data) {
-    const [user] = await db('users')
-      .select(['id', 'email'])
-      .where({ id: item.user_id });
-    item.user = user;
-    delete item.user_id;
+router
+  .route('/')
+  .get(auth.middleware, async (req, res) => {
+    const userID = req.auth.id;
+    const data = await db('campaigns').select('*');
+    for (const item of data) {
+      const [user] = await db('users')
+        .select(['id', 'email'])
+        .where({ id: item.user_id });
+      item.user = user;
+      delete item.user_id;
 
-    const [{ count: votes }] = await db('campaigns_votes')
-      .count('*')
-      .where({ campaign_id: item.id });
-    item.votes = +votes;
+      const [{ count: votes }] = await db('campaigns_votes')
+        .count('*')
+        .where({ campaign_id: item.id });
+      item.votes = +votes;
 
-    const [{ count: supported }] = await db('campaigns_votes')
-      .count('*')
-      .where({ campaign_id: item.id, user_id: userID });
-    item.supported = !!+supported;
-  }
-  return res.json(data);
-});
+      const [{ count: supported }] = await db('campaigns_votes')
+        .count('*')
+        .where({ campaign_id: item.id, user_id: userID });
+      item.supported = !!+supported;
+    }
+    return res.json(data);
+  })
+  .post(auth.middleware, async (req, res) => {
+    const userID = req.auth.id;
+    try {
+      const { title, description } = req.body;
+      await db('campaigns').insert({
+        title,
+        description,
+        user_id: userID,
+      });
+      res.send('Campaign created successfully!');
+    } catch (e) {
+      console.error(e);
+      return res.status(500).send('Server-side error: ' + e.message);
+    }
+  });
 
-router.get('/:id', auth.middleware, async (req, res) => {
+router.route('/:id').get(auth.middleware, async (req, res) => {
   const id = +req.params.id;
   const userID = req.auth.id;
   try {
@@ -57,27 +74,28 @@ router.get('/:id', auth.middleware, async (req, res) => {
   }
 });
 
-router.post('/:id/vote', auth.middleware, async (req, res) => {
-  const userID = req.auth.id;
-  const id = +req.params.id;
-  await db('campaigns_votes')
-    .insert({
+router
+  .route('/:id/vote')
+  .delete(auth.middleware, async (req, res) => {
+    const userID = req.auth.id;
+    const id = +req.params.id;
+    await db('campaigns_votes').delete().where({
       user_id: userID,
       campaign_id: id,
-    })
-    .onConflict()
-    .ignore();
-  res.send('Voted succesfully!');
-});
-
-router.delete('/:id/vote', auth.middleware, async (req, res) => {
-  const userID = req.auth.id;
-  const id = +req.params.id;
-  await db('campaigns_votes').delete().where({
-    user_id: userID,
-    campaign_id: id,
+    });
+    res.send('Vote removed succesfully!');
+  })
+  .post(auth.middleware, async (req, res) => {
+    const userID = req.auth.id;
+    const id = +req.params.id;
+    await db('campaigns_votes')
+      .insert({
+        user_id: userID,
+        campaign_id: id,
+      })
+      .onConflict()
+      .ignore();
+    res.send('Voted succesfully!');
   });
-  res.send('Vote removed succesfully!');
-});
 
 export default router;
