463dcc12128eb919009455eb5685a1ebe724ccb0
Param Siddharth <contact@paramsid.com>
2022-06-05 23:15:34 +0530

    Add text to about page
    Signed-off-by: Param Siddharth <contact@paramsid.com>

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,3 +1,24 @@
 <template>
-  <div>This is an about page.</div>
+  <div>
+    <h1>About Us</h1>
+    <p>
+      Hubbity is a volunteer hub for campaigners to gather support and receive
+      funds.
+    </p>
+    <p>
+      We were created with the intention to serve humanitarian campaigns,
+      focusing on the marginalized community. Sit ipsum nulla labore in. Officia
+      voluptate exercitation laborum pariatur elit ullamco exercitation deserunt
+      velit ex ex. Consectetur nulla eu ut elit nostrud minim aute minim veniam
+      in in occaecat laborum. Est adipisicing velit laborum do consectetur
+      nostrud fugiat ex aute consequat ad. In est id labore elit ad anim Lorem.
+      Exercitation est in adipisicing pariatur enim dolore anim pariatur.
+      Deserunt culpa deserunt amet aliqua enim irure ea veniam duis officia.
+      Elit id nisi quis do consectetur ullamco veniam aliqua. Laborum aute
+      cillum aliqua eu reprehenderit irure eu ut eiusmod. Laboris ex veniam
+      dolor consectetur aliqua nisi fugiat et culpa ad deserunt amet duis. Non
+      consequat magna cupidatat commodo aute eu minim sit ipsum ipsum consequat
+      ipsum.
+    </p>
+  </div>
 </template>
